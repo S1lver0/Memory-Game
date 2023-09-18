d8e0b5022c7cbec72c819771095f4bda799c944b
Yahir Choquehuanca <yahirchoquehuanca@gmail.com>
2023-09-18 13:35:42 -0500

    add event cards

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
+
 import "./Cards.css";
-export function Card({imagen}) {
+export function Card({ imagen }) {
+  var imagenTrasera = "src/assets/card-back.jpeg";
+  const [flipped, setFlipped] = useState(false);
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
   console.log(imagen);
   return (
-    <div className="card">
-      <img src={imagen} alt="Imagen" />
+    <div className="card-container">
+      <div className={`card ${flipped ? "flipped" : ""}`} onClick={handleClick}>
+        <div className="card-front">
+          {/* Contenido de la cara frontal */}
+          <img src={imagen} alt="Imagen" />
+        </div>
+        <div className="card-back">
+          <img src={imagenTrasera} alt="Imagen trasera" />
+        </div>
+      </div>
     </div>
   );
 }
